adf4482c35b9407c0059316774d4d6d2612a65f3
Treycip <treyci.perez@tecsup.edu.pe>
2023-09-05 18:45:02 -0500

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
+
+app.use(express.json());
 app.get('/',(req,res)=> {
     res.sendFile('./static/index.html',{
         root:__dirname
     })
 })
-
+app.get('/about',(req,res)=>{
+    res.json({
+        username: 'Treyci',
+        lastname: 'Perez'
+    })
+})
+  
+  
 app.listen(3000)
 console.log(`Server on port ${3000}`);
